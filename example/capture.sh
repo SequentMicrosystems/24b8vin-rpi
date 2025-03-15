87fe768ed10cc91a556983dc8ad40c4b8cc5590f
Alexandru Burcea <olcitu@gmail.com>
2025-03-15 02:59:44 -0700

    Update capture.sh

--- example/capture.sh
+++ example/capture.sh
@@ -1,11 +1,11 @@
 
-#set sample rate to 2000 samples per second, this parameter must be set once because is saved to the card flash memory
+#set sample rate to 2000 samples per second, this parameter must be set only once because it is saved to the card flash memory
 24b8vin 0 srwr 3 
 
-#setup the capture parameters for channel 1 of 4000 of samples when input voltage exceed 1.23V
+#setup the capture parameters for channel 1 of 4000  samples when the input voltage exceeds 1.23V
 24b8vin 0 scfgwr 1 4000 1.23 
 
-#begin monitoring the input. If you wish to start the capture imediatelly run "24b8vin 0 start 1"
+#begin monitoring the input. If you wish to start the capture immediately, run "24b8vin 0 start 1"
 24b8vin 0 start 0 
 
 a=0
@@ -15,7 +15,7 @@ do
     a=$(24b8vin 0 fifolvl)
 done
 echo "Done capture, start to read samples"
-#The card will save 
+#Save all the samples in a .csv file 
 for i in {0..4000} ; do 24b8vin 0 fiford ; done >> /home/pi/fifo-"`date +"%H:%M_%d-%m-%Y"`".csv
 
 echo "Done reading the samples"
